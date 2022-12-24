51e78759d4e57f4d77f2577602f778045dcbc426
Yusheng Wu <wuyusheng1023@gmail.com>
2022-12-24 21:58:56 +0200

    front talk to back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 
+
+const baseURL = process.env.ENDPOINT || 'http://localhost:8000';
+
+
+const getHelloWorld = async () => {
+  console.log('clicked "Hello"')
+
+  fetch(`${baseURL}/hello/`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then(responseJson => {
+    console.log(responseJson);
+  })
+  .catch(error => {
+    console.error(error);
+  })
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World!
-        </p>
+        <button onClick={getHelloWorld}>
+          Hello
+        </button>
       </header>
     </div>
   );
